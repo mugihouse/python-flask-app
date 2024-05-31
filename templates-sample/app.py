1c28a4dd9ab2e56158109c7267e1478f94d00359
mugihouse <mugi.stnk100122@gmail.com>
2024-05-31 23:05:40 +0900

    Add: ルーティング追加

---
+++ templates-sample/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('top.html')
+
+@app.route('/list')
+def item_list():
+  return render_template('list.html')
+
+@app.route('/detail')
+def item_detail():
+  return render_template('detail.html')
+
+if __name__ == '__main__':
+  app.run(port=8000)
