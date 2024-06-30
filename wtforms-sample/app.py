9fd16829103f5c68b40503ba1548a6d458af783e
mugihouse <mugi.stnk100122@gmail.com>
2024-06-30 21:56:50 +0900

    Change: バリデーションの結果によってルーティング先を変更

--- wtforms-sample/app.py
+++ wtforms-sample/app.py
@@ -7,9 +7,10 @@ from forms import UserInfoForm
 @app.route('/', methods=['GET', 'POST'])
 def show_enter():
   form = UserInfoForm(request.form)
-  if request.method == "POST":
-    pass
-  return render_template('enter.html', form=form)
+  if request.method == "POST" and form.validate():
+    return render_template('result.html', form=form)
+  # POST以外と「form.validate()がfalse」
+  return render_template('enter2.html', form=form)
 
 
 if __name__ == '__main__':
