d96afa278eb7a172e3b28690e9a38f180106390e
mugihouse <mugi.stnk100122@gmail.com>
2024-06-30 15:49:14 +0900

    Add: Formのルーティング追加

---
+++ form-sample/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/get')
+def do_get():
+  name = request.args.get('name')
+  return f'ハロー、{name}さん！'
+
+@app.route('/', methods=['GET', 'POST'])
+def do_get_post():
+  if request.method == 'POST':
+    name = request.form.get('name')
+    return f'こんにちは、{name}さん'
+  return '''
+    <h2>POSTで送信</h2>
+    <form method="post">
+      名前：<input type="text" name="name">
+      <input type="submit" value="送信">
+    </form>
+  '''
+
+if __name__ == '__main__':
+  app.run()
